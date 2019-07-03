5508184711a03b456cd90aaf4f144dcb4745ab7f
Mike Crimmins <crimmins@email.arizona.edu>
2019-07-03 02:01:57 +0000

    updates

--- vhi/analyzeNDVI.R
+++ vhi/analyzeNDVI.R
@@ -4,11 +4,15 @@
 
 library(raster)
 library(leaflet)
-
+library(leafem)
+library(RStoolbox)
 
 # CHANGE NDVI PRODUCT TYPE
 rasterData<-stack('/scratch/crimmins/vhi/processed/smNDVI_1982-2018_LCNCA.grd') # NDVI
 #rasterData<-stack('/scratch/crimmins/vhi/processed/VHI_1982-2018_LCNCA.grd')     # Vegetation Health Index
+
+# crop data
+# rasterData <- crop(rasterData, extent(-110.68451,-110.46822,31.644,31.94808))
 
 # get layer names and create date time series
 fileString<-as.data.frame(t(as.data.frame(strsplit((names(rasterData)),"[.]"))))
@@ -18,12 +22,18 @@ fileString<-as.data.frame(t(as.data.frame(strsplit((names(rasterData)),"[.]"))))
   fileString$week<-as.numeric(substr(fileString$yearWeek, 6,8))
   fileString$date<-as.Date(paste(fileString$year, fileString$week, 1, sep="-"), "%Y-%U-%u")
 
+# get full time series for period of record
+fullDate<-as.data.frame(seq.Date(fileString$date[1],fileString$date[nrow(fileString)], by="week"))
+  colnames(fullDate)<-"date"
+
 ## EXAMPLE: create time series of mean of each layer
   meanTimeSeries<-as.data.frame(cellStats(rasterData, mean))
     colnames(meanTimeSeries)<-c("meanValue")
     meanTimeSeries$date<-fileString$date
-    
-## EXAMPLE: Extract time series for one pixel based on lat/lon in grid extent for LCNCA
+    # merge with full dates to make serially complete time series
+    meanTimeSeries<-merge(meanTimeSeries, fullDate, by="date",all.y=TRUE)    
+        
+## EXAMPLE: Extract time series for one pixel based on lat/lon in grid extent for LCNCA -- NEEDS TO BE IN EXTENT OF RASTER DATA
     lat<-31.812430 
     lon<--110.589071
     
@@ -33,7 +43,7 @@ fileString<-as.data.frame(t(as.data.frame(strsplit((names(rasterData)),"[.]"))))
     
     
 ## EXAMPLE: Plot a layer on a leaflet map    
-    layer<-1
+    layer<-35
     
     pal <- colorNumeric(c("goldenrod4", "lightgoldenrod1", "springgreen4"), values(rasterData),
                         na.color = "transparent")
@@ -41,8 +51,11 @@ fileString<-as.data.frame(t(as.data.frame(strsplit((names(rasterData)),"[.]"))))
     leaflet() %>% addTiles() %>%
       addRasterImage(rasterData[[layer]], colors = pal, opacity = 0.8) %>%
       addLegend(pal = pal, values = values(rasterData),
-                title = "NDVI or VHI")
+                title = "NDVI or VHI") %>%
+      addMouseCoordinates()
     
     
+# EXAMPLE: Look at Principal Component Analysis
     
+    pcs<-rasterPCA(rasterData, nComp = 3)
     
--- vhi/subsetVHI.R
+++ vhi/subsetVHI.R
@@ -5,23 +5,25 @@
 library(raster)
 #library(stringr)
 #library(lubridate)
-library(plyr)
-library(grid)
-library(cowplot)
+# library(plyr)
+# library(grid)
+# library(cowplot)
 
 # LCNCA extent
 # -110.722275,31.642275,-110.422897,32.030781
+# AZ Extent
+# -114.938965,31.240985,-108.940430,37.099003
 
 # process with list of filenames
-#fileNames <- dir("/scratch/crimmins/vhi/smNDVI", "*.tif", full.names = TRUE)
- fileNames <- dir("/scratch/crimmins/vhi/VHItiff", "*.tif", full.names = TRUE)
+fileNames <- dir("/scratch/crimmins/vhi/smNDVI", "*.tif", full.names = TRUE)
+#fileNames <- dir("/scratch/crimmins/vhi/VHItiff", "*.tif", full.names = TRUE)
 # 
 # read layers into stack, by=1 for all weeks
 l<-1
   smNDVI <- stack()
   for (i in seq(1, length(fileNames), by=1)) {
     tempRast<-raster(fileNames[i])
-    e <- extent(-110.722275, -110.422897, 31.642275, 32.030781)
+    e <- extent(-114.938965,-108.940430,31.240985,37.099003)
     tempRast <- crop(tempRast, e)
     smNDVI <- stack(smNDVI, tempRast)
     print(names(tempRast))
@@ -31,4 +33,4 @@ l<-1
 smNDVI[smNDVI < 0] <- NA
 
 # write Raster to file
-writeRaster(smNDVI,filename='/scratch/crimmins/vhi/processed/VHI_1982-2018_LCNCA.grd', overwrite=TRUE)
+writeRaster(smNDVI,filename='/scratch/crimmins/vhi/processed/NDVI_1982-2018_AZ.grd', overwrite=TRUE)
